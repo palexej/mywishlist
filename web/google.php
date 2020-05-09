1b14975f1f5088a688175989f1b917ca17578e56
palexej <palexej@users.noreply.github.com>
2020-05-09 17:02:30 +0300

    google recapcha

--- web/google.php
+++ web/google.php
@@ -5,15 +5,55 @@
   <title></title>
 
   <script src="https://www.google.com/recaptcha/api.js?render=6LdH0_QUAAAAAEd5ihDpdwdJTcKh-LRGP2t07u6X"></script>
-  <script>
-  grecaptcha.ready(function() {
-    grecaptcha.execute('6LdH0_QUAAAAAEd5ihDpdwdJTcKh-LRGP2t07u6X', {action: 'homepage'}).then(function(token) {
-      ...
-    });
-  });
-</script>
+
 </head>
 <body>
+  <form method="post">
+    <input type="hidden" id="g-token" name="g-token" />
+    <button type="submit" name="subBtn"></button>
+
+  </form>
+
   ssss
 </body>
 </html>
+
+
+<?php
+if (isset($_POST['subBtn']))
+{
+  $secretKey='6LdH0_QUAAAAAC2Te54Q_xUz3tM0Czs7kOUsEwv9';
+  $token = $_POST["g-token"];
+  $ip = $_SERVER['REMOTE_ADDR'];
+  $url="https://www.google.com/recaptcha/api/siteverify".$secretKey;
+  $data = array('secret' => $secretKey, 'response' => $token, 'remoteip'=> $ip);
+
+  // use key 'http' even if you send the request to https://...
+  $options = array('http' => array(
+    'method'  => 'POST',
+    'content' => http_build_query($data)
+  ));
+  $context  = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+  $response = json_decode($result);
+  if($response->success)
+  {
+    echo '<center><h1>Validation Success!</h1></center>';
+  }
+  else
+  {
+    echo '<center><h1>Captcha Validation Failed..!</h1></center>';
+  }
+
+
+}
+?>
+
+
+<script>
+grecaptcha.ready(function() {
+  grecaptcha.execute('6LdH0_QUAAAAAEd5ihDpdwdJTcKh-LRGP2t07u6X', {action: 'homepage'}).then(function(token) {
+    document.getElementById("g-token").value = token;
+  });
+});
+</script>
