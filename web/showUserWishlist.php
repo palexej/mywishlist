d2b535f45a81aadc386147a650a44dea2dd82339
palexej <palexej@users.noreply.github.com>
2020-05-11 10:42:44 +0300

    обновил ссылки

--- web/showUserWishlist.php
+++ web/showUserWishlist.php
@@ -84,125 +84,130 @@
 <?php endif; ?>
 <br>
 <br>
-<div class="row ">
-  <div class="col-md-6 offset-md-3 ">
+<div class="container-fluid">
 
-    <?php
-    if (isset($_GET['ID']))
-    {
-      $wishlistID= $_GET['ID'];
-      $wishlists = R::load('newwishlists',$wishlistID);
-      $wishlistsCount=R::count('newwishlists','id=?',array($wishlistID));
 
-      if ($wishlistsCount<=0)
+  <div class="row ">
+    <div class="col-md-6 offset-md-3 ">
+
+      <?php
+      if (isset($_GET['ID']))
       {
-        echo '<meta http-equiv="refresh" content="0;url= https://awishlist.herokuapp.com/404.php "> ';
-      }
-      else
-      {
+        $wishlistID= $_GET['ID'];
+        $wishlists = R::load('newwishlists',$wishlistID);
+        $wishlistsCount=R::count('newwishlists','id=?',array($wishlistID));
 
-        $i=0;
-        $wishlistID=$wishlists->id;
-        $showInfo= $wishlists->wishlist_info;
-        $showData=  $wishlists->wishlist_data;
-        $showAddress = $wishlists->address;
+        if ($wishlistsCount<=0)
+        {
+          echo '<meta http-equiv="refresh" content="0;url= https://awishlist.herokuapp.com/404.php "> ';
+        }
+        else
+        {
 
-        $user_login_id=$wishlists->user_login_id;
-        $users = R::load('users',$user_login_id);
+          $i=0;
+          $wishlistID=$wishlists->id;
+          $showInfo= $wishlists->wishlist_info;
+          $showData=  $wishlists->wishlist_data;
+          $showAddress = $wishlists->address;
 
-        $userName=$users->login;
-        echo '
-        <br>
-        <h2 style="text-align:center">Список желаний пользователя '.$userName.' </h2>
-        <br>
+          $user_login_id=$wishlists->user_login_id;
+          $users = R::load('users',$user_login_id);
 
-        ';
-
-        echo '
-        <div class="card border-dark">
-        <div class="card-header" style="text-align:center">
-        <h5>'.$wishlists->wishlist_name.'</h5>
-        <div class="text-right">
-
-        </div>
-        </div>
-        <div class="card-body">
-        <div class="panel-group">
-        <div class="panel panel-default">
-
-
-        <ul class="list-group">
-        <li class="list-group-item"><b>Информация о мероприятии:</b> '.$showInfo.'</li>
-        <li class="list-group-item"><b>Дата проведения мероприятия:</b> '.$showData.'</li>
-        <li class="list-group-item"><b>Адрес проведения мероприятия:</b> '.$showAddress.'</li>
-
-        </ul>
-        ';
-        $wishs = R::find('wishs',"wish_wishlist_id=?",array($wishlistID));
-        $wishsCount = R::count('wishs',"wish_wishlist_id=?",array($wishlistID));
-
-        if ($wishsCount==0) {
+          $userName=$users->login;
           echo '
           <br>
-          <div>
-          <ul class="list-group">
-          <li class="list-group-item">В списке желаний пользователя '.$userName.' ещё нет желаний</li>
-          </ul>
-          </div>
+          <h2 style="text-align:center">Список желаний пользователя '.$userName.' </h2>
+          <br>
+
           ';
-        }
-        else {
-          // echo '<div class="card-desk">';
-          foreach ($wishs as $oneWish){
-            $oneWishId=$oneWish->id;
-            $oneWishName=$oneWish->wish_name;
-            $oneWishInfo=$oneWish ->wish_info;
-            $oneWishPrice=$oneWish->wish_price;
-            $oneWishLink=$oneWish->wish_link;
-            $oneWishImg=$oneWish->wish_img_path;
-            $oneWishCurrency =$oneWish->wish_currency_type;
 
-            // <ul class="list-group">
-            //<img class="card-img-top  w-50 h-50 p-3" src="cataloge-images/notebook.png" alt='.$i.' >
+          echo '
+          <div class="card border-dark">
+          <div class="card-header" style="text-align:center">
+          <h5>'.$wishlists->wishlist_name.'</h5>
+          <div class="text-right">
+
+          </div>
+          </div>
+          <div class="card-body">
+          <div class="panel-group">
+          <div class="panel panel-default">
+
+
+          <ul class="list-group">
+          <li class="list-group-item"><b>Информация о мероприятии:</b> '.$showInfo.'</li>
+          <li class="list-group-item"><b>Дата проведения мероприятия:</b> '.$showData.'</li>
+          <li class="list-group-item"><b>Адрес проведения мероприятия:</b> '.$showAddress.'</li>
+
+          </ul>
+          ';
+          $wishs = R::find('wishs',"wish_wishlist_id=?",array($wishlistID));
+          $wishsCount = R::count('wishs',"wish_wishlist_id=?",array($wishlistID));
+
+          if ($wishsCount==0) {
             echo '
-            <div class="card border-dark text-left" style="  display: inline-block">';
-            if ($oneWishImg!='') {
-
-              echo '
-              <div class="text-center">
-              <img class="card-img-top w-50 h-50 p-2 img-thumbnail" src="'.$oneWishImg.'">
-              </div>
-              ';
-            }
-            echo'
-
-            <div class="card-header"><b>Название желания:</b> '.$oneWishName.'</div>
-
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item"><b>Информация:</b> '.$oneWishInfo.'</li>
-            <li class="list-group-item"><b>Цена:</b> '.$oneWishPrice.''.$oneWishCurrency.'</li>
-            <li class="list-group-item"><b>Ссылка:</b> <a href='.$oneWishLink.' target="_blank">'.$oneWishLink.'</a></li>
-            </ul>
-            </div>
             <br>
-            <br>
+            <div>
+            <ul class="list-group">
+            <li class="list-group-item">В списке желаний пользователя '.$userName.' ещё нет желаний</li>
             </ul>
             </div>
             ';
-
           }
-        }
-        echo '
+          else {
+            // echo '<div class="card-desk">';
+            foreach ($wishs as $oneWish){
+              $oneWishId=$oneWish->id;
+              $oneWishName=$oneWish->wish_name;
+              $oneWishInfo=$oneWish ->wish_info;
+              $oneWishPrice=$oneWish->wish_price;
+              $oneWishLink=$oneWish->wish_link;
+              $oneWishImg=$oneWish->wish_img_path;
+              $oneWishCurrency =$oneWish->wish_currency_type;
 
-        </div>
-        </div>
-        </div>
-        ';
-        $i++;
+              // <ul class="list-group">
+              //<img class="card-img-top  w-50 h-50 p-3" src="cataloge-images/notebook.png" alt='.$i.' >
+              echo '
+              <div class="card border-dark text-left" style="  display: inline-block">';
+              if ($oneWishImg!='') {
+
+                echo '
+                <div class="text-center">
+                <img class="card-img-top w-50 h-50 p-2 img-thumbnail" src="'.$oneWishImg.'">
+                </div>
+                ';
+              }
+              echo'
+
+              <div class="card-header"><b>Название желания:</b> '.$oneWishName.'</div>
+
+              <ul class="list-group list-group-flush">
+              <li class="list-group-item"><b>Информация:</b> '.$oneWishInfo.'</li>
+              <li class="list-group-item"><b>Цена:</b> '.$oneWishPrice.''.$oneWishCurrency.'</li>
+              <li class="list-group-item"><b>Ссылка:</b> <a href='.$oneWishLink.' target="_blank">'.$oneWishLink.'</a></li>
+              </ul>
+              </div>
+              <br>
+              <br>
+              </ul>
+              </div>
+              ';
+
+            }
+          }
+          echo '
+
+          </div>
+          </div>
+          </div>
+          ';
+          $i++;
+
+        }
 
       }
+      ?>
 
-    }
-    ?>
+    </div>
   </body>
   </html>
