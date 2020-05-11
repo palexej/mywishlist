4d02d9dc4d5470a68cb65cb1409008b34304f089
palexej <palexej@users.noreply.github.com>
2020-05-11 10:41:02 +0300

    небольшие изменения

--- web/showUserWishlist.php
+++ web/showUserWishlist.php
@@ -8,7 +8,7 @@
 
   <style>
   body {
-    background: url(images-background/infowishlist.jpg) no-repeat center center fixed;
+    background: url(images-background/showUserWishlist.jpg) no-repeat center center fixed;
     -webkit-background-size: cover;
     -moz-background-size: cover;
     -o-background-size: cover;
