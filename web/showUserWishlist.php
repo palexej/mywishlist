2b65260b70c89822da7aa6c9d9a7a9c7de0e8ee6
palexej <palexej@users.noreply.github.com>
2020-05-15 12:43:33 +0300

    просмотр в пользовательском списке

--- web/showUserWishlist.php
+++ web/showUserWishlist.php
@@ -224,7 +224,8 @@
               $url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
               echo '
-
+              <br>
+              <div class="WishList">
               <h3  style="text-align:center">Осуществить желание пользователя</h3>
               <form  action='.$url.' method="post">
               <div class="form-group">
@@ -259,7 +260,7 @@
               <br>
               <button class="btn btn-success btn-block" type="submit" name="executeBtn">Выполнить желание</button>
               </form>
-
+              </div>
               ';
             }
             else
