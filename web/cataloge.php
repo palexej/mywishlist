be60c4b0d3b880e7c99fd05b0062683a12c990e6
palexej <palexej@users.noreply.github.com>
2020-05-16 16:45:20 +0300

    добавляю уууee

--- web/cataloge.php
+++ web/cataloge.php
@@ -324,33 +324,30 @@
     </div>
   </div>
 </div>
-<?php if ($productsCount!=0)
-{
-  echo '
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
-  <div class="modal-content">
-  <div class="modal-header">
-  <h5 class="modal-title" id="exampleModalLabel"></h5>
-  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-  <span aria-hidden="true">&times;</span>
-  </button>
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p id="ID_prod_info"></p>
+        <p id="ID_prod_type"></p>
+        <p id="ID_prod_price"></p>
+        <p id="ID_prod_link"></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+      </div>
+    </div>
   </div>
-  <div class="modal-body">
-  <p id="ID_prod_info"></p>
-  <p id="ID_prod_type"></p>
-  <p id="ID_prod_price"></p>
-  <p id="ID_prod_link"></p>
-  </div>
-  <div class="modal-footer">
-  <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
-  </div>
-  </div>
-  </div>
-  </div>
-  </div>';
-}
-?>
+</div>
+</div>
+
 
 </body>
 </html>
