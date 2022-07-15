a07877a95a5f04ec916dc7ea51eb689549213b4b
palexej <57910063+palexej@users.noreply.github.com>
2022-07-15 18:23:05 +0300

    Update db.php

--- web/db.php
+++ web/db.php
@@ -1,6 +1,6 @@
 <?php
 require 'libs/rb.php';
-R::setup( 'mysql:host=us-cdbr-east-06.cleardb.net; dbname=heroku_8577067324d828a','***', '***' );
+R::setup( 'mysql:host=us-cdbr-east-06.cleardb.net; dbname=heroku_8577067324d828a','be0fd66079ab9f', 'ed9d44bd' );
 
 if ( !R::testconnection() )
 {
