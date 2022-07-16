d6f39048830f84cc9e253c18fc81b01f06219efa
palexej <57910063+palexej@users.noreply.github.com>
2022-07-16 09:54:18 +0300

    Update createWishList.php

--- web/createWishList.php
+++ web/createWishList.php
@@ -15,7 +15,7 @@
 	?>
 	<style>
 	body {
-		background: url(images-background/birthday-pic.jpg) no-repeat center center fixed;
+		background: url(https://random.imagecdn.app/1920/1080) no-repeat center center fixed;
 		-webkit-background-size: cover;
 		-moz-background-size: cover;
 		-o-background-size: cover;
