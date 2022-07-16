e63cd63617cb3c26fe78c18ff9b7b8727c6de2e4
palexej <57910063+palexej@users.noreply.github.com>
2022-07-16 09:54:32 +0300

    Update editMyWishInfo.php

--- web/editMyWishInfo.php
+++ web/editMyWishInfo.php
@@ -6,7 +6,7 @@
   <?php require_once 'dependence.php' ?>
   <style>
   body {
-    background: url(images-background/flowers-present-pic.jpg) no-repeat center center fixed;
+    background: url(https://random.imagecdn.app/1920/1080) no-repeat center center fixed;
     -webkit-background-size: cover;
     -moz-background-size: cover;
     -o-background-size: cover;
