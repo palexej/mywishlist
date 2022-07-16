4c6ca23164a6f7273bace0eb61e047a4c5505023
palexej <57910063+palexej@users.noreply.github.com>
2022-07-16 09:55:45 +0300

    Update upload.php

--- web/upload.php
+++ web/upload.php
@@ -17,7 +17,7 @@
 
   <style>
   body {
-    background: url(images-background/infowishlist.jpg) no-repeat center center fixed;
+    background: url(https://random.imagecdn.app/1920/1080) no-repeat center center fixed;
     -webkit-background-size: cover;
     -moz-background-size: cover;
     -o-background-size: cover;
